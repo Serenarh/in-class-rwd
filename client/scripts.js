fa7b081d812b0c079df945ba9b58615f7ed604a3
Serena Hasler <srhasler@yahoo.com>
2020-09-11 15:17:53 -0500

    Make changes to match class instructor files

--- client/scripts.js
+++ client/scripts.js
@@ -1,27 +1,32 @@
-//import {test} from validator.mjs
+import { test } from "./validator.mjs";
+
+console.log(test());
 
 const nav = document.querySelector("nav");
 const inputs = document.querySelectorAll("input");
 
-document.querySelector(".fa-hamburger").addEventListener("click",
+document.querySelector(".fa-hamburger").addEventListener(
+  "click",
   // Callback fxn.
   () => {
     // Short-circuit OR operator
     nav.classList.replace("is-collapsed", "is-expanded") ||
-      nav.classList.replace("is-expanded", "is-collapsed");})
+      nav.classList.replace("is-expanded", "is-collapsed");
+  }
+);
 
-inputs.forEach(input => {
-  input.addEventListener('blur', (event) => {
-    const currentInput = event.target
-    const regex = new RegExp(currentInput.pattern)
+inputs.forEach((input) => {
+  input.addEventListener("blur", () => {
+    const currentInput = event.target;
+    const regex = new RegExp(currentInput.pattern);
 
-//If 'not true' that regex passes or not true that there is a value
-    if (!regex.test(currentInput.value.toUpperCase) || !currentInput.value)
-    {
-      currentInput.classList.replace('is-success', 'is-error') ||currentInput.classList.add('is-error')
-    } else{
-      currentInput.classList.replace('is-error', 'is-success') ||currentInput.classList.add('is-success')
+    // If 'not true' that regex passes or not true that there is a value
+    if (!regex.test(currentInput.value.toUpperCase()) || !currentInput.value) {
+      currentInput.classList.replace("is-success", "is-error") ||
+        currentInput.classList.add("is-error");
+    } else {
+      currentInput.classList.replace("is-error", "is-success") ||
+        currentInput.classList.add("is-success");
     }
-  })
-})
-
+  });
+});
